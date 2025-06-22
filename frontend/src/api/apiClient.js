5cd61d18a7c014c509f8991814577b55111556ba
Matt Edwards <mattsp.edwards@googlemail.com>
2025-06-22 16:16:27 +0100

    feat: added apiClient for axios

---
+++ frontend/src/api/apiClient.js
@@ -0,0 +1,38 @@
+import axios from "axios";
+import { auth } from "@/auth"
+
+const apiClient = () => {
+    const defaultOptions = {
+        baseURL: `${process.env.NEXT_PUBLIC_API_HOST}/api/`,
+        headers: {
+            "Content-Type": "application/json",
+            accept: "application/json",
+        },
+    }
+    
+    const instance = axios.create(defaultOptions);
+
+    instance.interceptors.request.use(async (request) => {
+        const session = await auth()
+        if (session) {
+            request.headers.Authorization = `Bearer ${session.access_token}`;
+        }
+        return request;
+    })
+
+    instance.interceptors.response.use(
+        (response) => {
+            return response;
+        },
+        (error) => {
+            if (error.response.status === 401) {
+                window.location.href = '/';
+            }
+            return Promise.reject(error)
+        }
+    )
+
+    return instance;
+}
+
+export default apiClient();
