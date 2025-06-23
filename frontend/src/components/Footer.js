2807fdfb322fde191869611274efd81a4b395bd0
Matt Edwards <mattsp.edwards@googlemail.com>
2025-06-23 22:49:31 +0100

    feat: added simple footer

---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import { Box, Typography } from '@mui/material';
+
+export default function Footer() {
+    return (
+        <Box
+            component="footer"
+            sx={{
+                position: 'fixed',
+                bottom: 0,
+                left: 0,
+                width: '100%',
+                py: 2,
+                px: 2,
+                backgroundColor: 'background.paper',
+                textAlign: 'center',
+            }}
+        >
+            <Typography variant="body2" color="text.secondary">
+                &copy; {new Date().getFullYear()} Redactor. All rights reserved.
+            </Typography>
+        </Box>
+    )
+}
