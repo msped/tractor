87b432b5253941ee82dc8aa2ef9f5bca11a670d4
Matt Edwards <mattsp.edwards@googlemail.com>
2025-06-26 22:37:58 +0100

    feat: added cases page to show a list of cases

--- frontend/src/app/cases/page.js
+++ frontend/src/app/cases/page.js
@@ -1,15 +1,43 @@
 import React from 'react'
 import { auth } from "@/auth"
 import { redirect } from "next/navigation"
+import { Box, Typography, Container } from '@mui/material';
+import apiClient from '@/api/apiClient';
+
+import DataTable from '@/components/DataTable';
 
 export default async function page() {
     const session = await auth();
-    
+
     if (!session) {
         redirect("/");
     }
 
+    let cases = [];
+    let fetchError = null;
+
+    try {
+        cases = await apiClient.get('cases').then(response => response.data);
+        console.log("Fetched cases:", cases);
+    } catch (error) {
+        console.error("Failed to fetch cases:", error);
+        fetchError = "There was an issue retrieving your cases. Please try again later.";
+    }
+
     return (
-        <div>page</div>
+        <Container maxWidth="lg" sx={{ py: 4 }}>
+            <Box sx={{ textAlign: 'center', mb: 4 }}>
+                <Typography variant="h4" component="h1" gutterBottom>
+                    Cases
+                </Typography>
+            </Box>
+            <Box sx={{ height: 600, width: '100%', my: 4 }}>
+                {fetchError ? (
+                    <Typography color="error">{fetchError}</Typography>
+                ) : (
+                    <DataTable rows={cases}/>
+                )}
+            </Box>
+        </Container>
     )
 }
