217ff4363b8ef1736562b61f5a79ee47a4441877
Matt Edwards <mattsp.edwards@googlemail.com>
2025-06-29 21:34:54 +0100

    fix: temp remove of session check as caused loop

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,14 +1,7 @@
-import { redirect } from "next/navigation";
-import { auth } from "@/auth"
 import LoginComponent from "@/components/LoginComponent";
 
 export default async function Home() {
-  const session = await auth();
 
-  if (session) {
-      redirect("/cases");
-    return null;
-  }
 
   return <LoginComponent />;
 }
