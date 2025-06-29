d49e2fc0b64d7b5168e4b62b9cf8c11ce2fbe645
Matt Edwards <mattsp.edwards@googlemail.com>
2025-06-29 21:36:14 +0100

    feat: created a Case page to fetch data from server and pass to component for display

---
+++ frontend/src/app/cases/[id]/page.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import apiClient from '@/api/apiClient';
+import { auth } from "@/auth"
+import { redirect } from "next/navigation"
+import { Container, Stack } from '@mui/material';
+
+import CaseInformation from '@/components/CaseInformation';
+import CaseDocuments from '@/components/CaseDocuments';
+
+export default async function page({ params }) {
+    const { id } = await params;
+    const session = await auth();
+
+    let caseFile = null;
+    let fetchError = null;
+
+    try {
+        caseFile = await apiClient.get(`cases/${id}`, {
+            headers: {
+                Authorization: `Bearer ${session.access_token}`,
+            },
+        }).then(response => response.data)
+    } catch (error) {
+        if (error.response?.status === 401) {
+            redirect('/');
+        }
+        console.error("Failed to fetch cases:", error);
+        fetchError = "There was an issue retrieving your cases. Please try again later.";
+    }
+
+    return (
+        <Container maxWidth="lg" sx={{ py: 4 }}>
+            <Stack direction="column" spacing={2}>
+                <CaseInformation caseObject={caseFile} />
+                <CaseDocuments caseId={id} documents={caseFile?.documents}/>
+            </Stack>
+        </Container>
+    )
+}
