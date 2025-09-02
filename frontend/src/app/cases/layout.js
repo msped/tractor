82e9860555ea813ae615dfcedb2a4374396d2e5a
Matt Edwards <mattsp.edwards@googlemail.com>
2025-09-03 00:19:59 +0100

    refactor: moved session provider, header, and footer to main layout

--- frontend/src/app/cases/layout.js
+++ frontend/src/app/cases/layout.js
@@ -1,17 +1,10 @@
-import Header from "@/components/Header";
-import Footer from "@/components/Footer";
 import Box from "@mui/material/Box";
-import { SessionProvider } from "next-auth/react";
 
 export default function RootLayout({ children }) {
 
     return (
-        <SessionProvider>
-            <Header/>
-            <Box sx={{ minHeight: '90vh' }}>
-                {children}
-            </Box>
-            <Footer />
-        </SessionProvider>
+        <Box sx={{ minHeight: '90vh' }}>
+            {children}
+        </Box>
     );
 }
