b27747b912408d5ae2e654d069ce897be3df987e
Matt Edwards <mattsp.edwards@googlemail.com>
2025-09-03 01:12:47 +0100

    feat: added views for models available for use

--- training/views.py
+++ training/views.py
@@ -1,3 +1,44 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.generics import (
+    ListCreateAPIView,
+    RetrieveUpdateDestroyAPIView
+)
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.permissions import IsAdminUser
+from django.shortcuts import get_object_or_404
+from .models import Model
+from .serializers import ModelSerializer
 
-# Create your views here.
+
+class ModelListCreateView(ListCreateAPIView):
+    """
+    API view to list all trained models or create a new model entry.
+    """
+    permission_classes = [IsAdminUser]
+    queryset = Model.objects.all().order_by('-created_at')
+    serializer_class = ModelSerializer
+
+
+class ModelDetailView(RetrieveUpdateDestroyAPIView):
+    """
+    API view to retrieve, update, or delete a specific model.
+    """
+    permission_classes = [IsAdminUser]
+    queryset = Model.objects.all()
+    serializer_class = ModelSerializer
+    lookup_field = 'pk'
+
+
+class ModelSetActiveView(APIView):
+    """API view to set a model as active."""
+    permission_classes = [IsAdminUser]
+
+    def post(self, request, pk, *args, **kwargs):
+        model = get_object_or_404(Model, pk=pk)
+        model.is_active = True
+        model.save()
+        return Response(
+            {'status': 'model set to active'},
+            status=status.HTTP_200_OK
+        )
