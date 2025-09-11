c817b81be380ae0c40fc54c597940f724f3b602e
Matt Edwards <mattsp.edwards@googlemail.com>
2025-09-12 00:14:31 +0100

    fix: added model entry to assist in FK declartion in Document model

--- training/loader.py
+++ training/loader.py
@@ -11,6 +11,7 @@ class SpacyModelManager:
     def __init__(self):
         self.model = None
         self.model_name = None
+        self.model_entry = None
         self.load_active_model()
 
     @classmethod
@@ -23,10 +24,13 @@ class SpacyModelManager:
     def load_model(self, path, name=None):
         self.model = spacy.load(path)
         self.model_name = name or path
+        self.model_entry = SpacyModel.objects.filter(
+            name=self.model_name).first()
 
     def load_active_model(self):
         active_model = SpacyModel.objects.filter(is_active=True).first()
         if active_model:
+            self.model_entry = active_model
             self.load_model(active_model.path, active_model.name)
 
     def get_model(self):
@@ -34,11 +38,14 @@ class SpacyModelManager:
             self.load_active_model()
         return self.model
 
+    def get_model_entry(self):
+        return self.model_entry
+
     def switch_model(self, model_name):
         model_entry = SpacyModel.objects.get(name=model_name)
         self.load_model(model_entry.path, model_entry.name)
 
         # Set as active
-        SpacyModel.objects.update(is_active=False)
-        model_entry.is_active = True
-        model_entry.save()
+        if not model_entry.is_active:
+            model_entry.is_active = True
+            model_entry.save(update_fields=['is_active'])
