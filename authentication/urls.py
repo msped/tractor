829a7a798bec0afaa1eb86956d98ee0812426c75
Matt Edwards <mattsp.edwards@googlemail.com>
2025-12-16 17:33:20 +0000

    feat: added microsoft entra id auth from django allauth

--- authentication/urls.py
+++ authentication/urls.py
@@ -3,6 +3,7 @@ from dj_rest_auth.jwt_auth import get_refresh_view
 from dj_rest_auth.views import LoginView, LogoutView, UserDetailsView
 from django.urls import path
 from rest_framework_simplejwt.views import TokenVerifyView
+from .views import MicrosoftLogin
 
 urlpatterns = [
     path("login", LoginView.as_view(), name="rest_login"),
@@ -10,4 +11,9 @@ urlpatterns = [
     path("user", UserDetailsView.as_view(), name="rest_user_details"),
     path("token/verify", TokenVerifyView.as_view(), name="token_verify"),
     path("token/refresh", get_refresh_view().as_view(), name="token_refresh"),
+    path(
+        'microsoft',
+        MicrosoftLogin.as_view(),
+        name='microsoft_login'
+    ),
 ]
