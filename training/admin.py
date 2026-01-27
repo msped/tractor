bf3bacde15812538fe532ee6944115af9c76b35a
Matt Edwards <mattsp.edwards@googlemail.com>
2026-01-27 23:40:29 +0000

    feat: added TrainingDocument and TrainingRun to tasks/admin.py

--- training/admin.py
+++ training/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 
-from .models import Model
+from .models import Model, TrainingRun, TrainingDocument
 
 # This makes the Schedule model from django-q2 and our
 # new Model appear in your admin site.
 admin.site.register(Model)
+admin.site.register(TrainingDocument)
+admin.site.register(TrainingRun)
