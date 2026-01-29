8077147c0c045ac621c36f6836b72958a79fc95a
Matt Edwards <mattsp.edwards@googlemail.com>
2026-01-29 23:50:49 +0000

    feat: added training documents to the training run django admin as tab inline

--- training/admin.py
+++ training/admin.py
@@ -1,9 +1,35 @@
 from django.contrib import admin
 
-from .models import Model, TrainingRun, TrainingDocument
+from .models import Model, TrainingDocument, TrainingRun, TrainingRunCaseDoc, TrainingRunTrainingDoc
 
-# This makes the Schedule model from django-q2 and our
-# new Model appear in your admin site.
+
+class TrainingRunTrainingDocInline(admin.TabularInline):
+    model = TrainingRunTrainingDoc
+    extra = 0
+    readonly_fields = ["document"]
+    can_delete = False
+
+    def has_add_permission(self, request, obj=None):
+        return False
+
+
+class TrainingRunCaseDocInline(admin.TabularInline):
+    model = TrainingRunCaseDoc
+    extra = 0
+    readonly_fields = ["document"]
+    can_delete = False
+
+    def has_add_permission(self, request, obj=None):
+        return False
+
+
+@admin.register(TrainingRun)
+class TrainingRunAdmin(admin.ModelAdmin):
+    list_display = ["id", "model", "source", "created_at"]
+    list_filter = ["source", "created_at"]
+    readonly_fields = ["id", "model", "source", "created_at"]
+    inlines = [TrainingRunTrainingDocInline, TrainingRunCaseDocInline]
+
+
 admin.site.register(Model)
 admin.site.register(TrainingDocument)
-admin.site.register(TrainingRun)
